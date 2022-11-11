2add3eced88e8e3951192ecd7b8665d0bf2a62a6
Lixuan Yang <36718986+LixuanYang@users.noreply.github.com>
2022-11-11 16:49:34 -0500

    Delete LF0.py

--- lambdas/LF0.py
+++
@@ -1,9 +0,0 @@
-def lambda_handler(event, context):
-     return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Accept,x-amz-meta-customLabels',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS,PUT,GET'
-        }
-    }
