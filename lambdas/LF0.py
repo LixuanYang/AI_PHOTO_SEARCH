ef86b482bcf9822f61a6fe57fd3eaf133159c2d2
Lixuan Yang <36718986+LixuanYang@users.noreply.github.com>
2022-11-11 11:26:12 -0500

    Add files via upload

---
+++ lambdas/LF0.py
@@ -0,0 +1,9 @@
+def lambda_handler(event, context):
+     return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Accept,x-amz-meta-customLabels',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,PUT,GET'
+        }
+    }
